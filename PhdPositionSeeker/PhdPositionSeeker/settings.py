c0f2f13df0cbb49b8e8effce5732d6b86ce8945a
wb <519369865@qq.com>
2015-07-16 09:11:26 +0800

    init spider framework(scrapy)

---
+++ PhdPositionSeeker/PhdPositionSeeker/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for PhdPositionSeeker project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'PhdPositionSeeker'
+
+SPIDER_MODULES = ['PhdPositionSeeker.spiders']
+NEWSPIDER_MODULE = 'PhdPositionSeeker.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'PhdPositionSeeker (+http://www.yourdomain.com)'
